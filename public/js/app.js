5d0469fd707f17af15f33745181ac6a7f04d14f7
fahad <muhammad.fahad@techverx.com>
2021-06-04 16:30:36 -0400

    Setup/Changes in app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,8 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    fetch('http://localhost:3000/weather?search='+location).then((response) => {
+    //fetch('http://localhost:3000/weather?search='+location).then((response) => { //for localhost setup
+    fetch('/weather?search='+location).then((response) => {
         response.json().then((data)=>{
             if(data.error){
                 messageOne.textContent = data.error
